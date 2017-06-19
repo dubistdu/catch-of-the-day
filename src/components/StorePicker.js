158bbf3f824044e02dea70a7d635063806156e8a
Hyun Frantz <hyunfrantz@gmail.com>
2017-06-19 03:29:51 -0400

    writing html/commenting out  within react

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -3,7 +3,17 @@ import React from 'react';
 
 class StorePicker extends React.Component {
   render(){
-     return <p>'Hello'</p>
+     return (
+       <form className="store-selector">
+       {/* to comment out within jsx, you can't do // use {/* comment*/}
+          {/*comment location matters*/}
+        <h2>Please enter a store</h2>
+        <input type="text" required placeholder="Store Name" />
+        <button type="submit">Visit store</button>
+       </form>
+     ) //all html needed will go inside (). since class is reserved by js, className will be used
+       //short cut -> form.name of the class
+       //things like img tag that did not need closing tag in html needs to self close here
   }
 }
 
