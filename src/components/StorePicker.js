4353189d33c7a6006f0b4c6aaafef18e6bb3fc4f
Hyun Frantz <hyunfrantz@gmail.com>
2017-06-19 02:46:15 -0400

    created React component 'StorePicker'

---
+++ src/components/StorePicker.js
@@ -0,0 +1,12 @@
+//do import React from 'react'; to every file that needs react
+import React from 'react';
+
+class StorePicker extends React.Component {
+  render(){
+     return <p>'Hello'</p>
+  }
+}
+
+export default StorePicker;
+
+//We've created a component 'StorePciker'
--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 // let's go!
+import React from 'react';
+
+// this imports only one element from react dom. import ReactDom from 'react-dom' import the whole React library
+import { render } from 'react-dom';
+
+import StorePicker from './components/StorePicker'
+
+//jsx '<' open Component, '/>' close Component
+//second argument. What Dom element should render out to
+render (<StorePicker/>, document.querySelector('#main'));
