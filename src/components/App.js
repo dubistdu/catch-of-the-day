e4edfc5f050046390dd446e8f0e8bb89e26ba707
Hyun Frantz <hyunfrantz@gmail.com>
2017-06-20 02:30:29 -0400

    creating app layers with component

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import Header from './Header.js';
+import Header from './Header';
+import Inventory from './Inventory';
+import Order from './Order'
 
-class App extends React.component {
+class App extends React.Component {
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header />
         </div>
-          <Order />
-          <Inventory />
+        <Order />
+        <Inventory/>
       </div>
     )
   }
