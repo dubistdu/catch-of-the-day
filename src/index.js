61e44bbe45e7f3e115af1ef4eab04fb53e1d0ac1
Hyun Frantz <hyunfrantz@gmail.com>
2017-06-23 23:56:21 -0400

    React Router

--- src/index.js
+++ src/index.js
@@ -2,14 +2,29 @@
 import React from 'react';
 
 // this imports only one element from react dom. import ReactDom from 'react-dom' import the whole React library
-import { render } from 'react-dom';
+import{ render } from 'react-dom';
+
+//Router setting. Even a router is a component in React
+import{ BrowserRouter, Match, Miss } from 'react-router';
 
 import StorePicker from './components/StorePicker';
 import './css/style.css';
 import App from './components/App';
 
+const Root = () => {
+  return(
+    <BrowserRouter>
+       //Match can't be a direct child of BrowserRouter. Hence div
+      <div>
+        <Match exactly pattern="/" component={StorePicker}/>
+        <Match pattern="/store/:storeId" component={App}/>
+      </div>
+    </BrowserRouter>
+  )
+}
+
 //jsx '<' open Component, '/>' close Component
 //second argument. What Dom element should render out to
-render (<App/>, document.querySelector('#main'));
+render (<Root/>, document.querySelector('#main'));
 
 //render (<name Of components that render/>);
