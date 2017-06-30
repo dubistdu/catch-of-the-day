22e48ebbf05f4619df689b6b1a04d98c43a7efe9
Hyun Frantz <hyunfrantz@gmail.com>
2017-06-29 23:56:22 -0400

    fixed typo

--- src/components/AddFishForm.js
+++ src/components/AddFishForm.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
 class AddFishForm extends React.Component {
+  createFish(event){
+    event.preventDefault();
+    console.log('Gonna make some fish');
+  }
+
   render(){
     return(
-      <form className="fish-edit">
+      <form className="fish-edit" onSubmit={(e)=>this.createFish(e)}>
         <input type="text" placeholder="Fish Name" />
         <input type="text" placeholder="Fish Price" />
         <select>
@@ -12,7 +17,7 @@ class AddFishForm extends React.Component {
         </select>
         <textarea placeholder="Fish Desc"></textarea>
         <input type="text" placeholder="Fish Image" />
-        <button type="submit>+ Add Item"</button>
+        <button type="submit">+ Add Item</button>
       </form>
     )
   }
